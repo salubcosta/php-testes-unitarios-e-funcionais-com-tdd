242b478adc0e7419af5a6b716fd1793e5d593ccc
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-04-04 23:34:26 -0300

    Classe Product para começar a testar

---
+++ tdd/src/Product.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Code;
+
+class Product
+{
+    private $name;
+    private $price;
+    private $slug;
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
