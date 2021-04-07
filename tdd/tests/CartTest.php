f3c616cf58a2a6b94dabc3267797f611f32ec269
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-04-06 23:36:05 -0300

    metodos setUp e tearDown

--- tdd/tests/CartTest.php
+++ tdd/tests/CartTest.php
@@ -6,6 +6,22 @@ use PHPUnit\Framework\TestCase;
 
 class CartTest extends TestCase
 {
+    private $cart;
+    private $product;
+
+    // É excutado antes de cada teste
+    public function setUp(): void 
+    {
+        $this->cart = new Cart();
+        $this->product = new Product();
+    }
+
+    // É excutado depois de cada teste
+    public function tearDown(): void
+    {
+        unset($this->cart, $this->product);
+    }
+
     public function testClasseCarrinhoExiste()
     {
         $class = class_exists('\\Code\\Cart');
@@ -15,17 +31,17 @@ class CartTest extends TestCase
 
     public function testAdicaoDeProdutosNoCarrinho()
     {
-        $product_01 = (new Product)
+        $product_01 = ($this->product)
             ->setName('Product 01')
             ->setPrice(200)
             ->setSlug('product-01');
 
-        $product_02 = (new Product)
+        $product_02 = ($this->product)
             ->setName('Product 01')
             ->setPrice(200)
             ->setSlug('product-01');
         
-        $cart = (new Cart())->addProduct($product_01)->addProduct($product_02);
+        $cart = ($this->cart)->addProduct($product_01)->addProduct($product_02);
 
         $this->assertIsArray($cart->getProducts());
 
@@ -34,12 +50,12 @@ class CartTest extends TestCase
 
     public function testSeValoresDeProdutoNoCarrinhoEstaoCorretosConformePassados()
     {
-        $product = (new Product)
+        $product = ($this->product)
             ->setName('Product 01')
             ->setPrice(200)
             ->setSlug('product-01');
 
-        $cart = (new Cart())->addProduct($product);
+        $cart = ($this->cart)->addProduct($product);
 
         $this->assertEquals('Product 01', $cart->getProducts()[0]->getName());
         $this->assertEquals(200, $cart->getProducts()[0]->getPrice());
@@ -48,17 +64,17 @@ class CartTest extends TestCase
 
     public function testSeTotalDeProdutosEValorDaCompraEstaoCorretos()
     {
-        $product_01 = (new Product)
+        $product_01 = ($this->product)
             ->setName('Product 01')
             ->setPrice(200)
             ->setSlug('product-01');
 
-        $product_02 = (new Product)
+        $product_02 = ($this->product)
             ->setName('Product 01')
             ->setPrice(200)
             ->setSlug('product-01');
         
-        $cart = (new Cart())->addProduct($product_01)->addProduct($product_02);
+        $cart = ($this->cart)->addProduct($product_01)->addProduct($product_02);
 
         $this->assertEquals(2, $cart->getTotalProducts());
         $this->assertEquals(400, $cart->getTotalPurchases());
