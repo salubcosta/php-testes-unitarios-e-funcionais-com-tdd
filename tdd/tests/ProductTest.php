a1339da3810806d6719d8ca7efb671a7b72b67cf
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-04-06 23:36:43 -0300

    Metodos setUp, tearDown, setUpBeforeClsas e tearDownAfterclass

--- tdd/tests/ProductTest.php
+++ tdd/tests/ProductTest.php
@@ -6,23 +6,49 @@ use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
+    private $product;
+
+    // É excutado antes de cada teste 
+    public function setUp(): void
+    {
+        $this->product = new Product();
+    }
+
+    // É excutado depois de cada teste 
+    public function tearDown(): void
+    {
+        unset($this->product);
+    }
+    
+    // É executado apenas uma vez antes de cada teste da classe
+    public static function setUpBeforeClass(): void
+    {
+        print __METHOD__;
+    }
+
+    // É executado apenas uma vez depois de cada teste da classe
+    public static function tearDownAfterClass(): void
+    {
+        print __METHOD__;
+    }
+
     public function testNomeSetadoCorretamente()
     {
-        $product = (new Product())->setName('Product 01');
+        $product = ($this->product)->setName('Product 01');
         
         $this->assertEquals('Product 01', $product->getName(), "Values aren't equals");
     }
 
     public function testPriceSetadoCorretamente()
     {
-        $product = (new Product())->setPrice(100.9);
+        $product = ($this->product)->setPrice(100.9);
         
         $this->assertEquals(100.9, $product->getPrice(), "Values aren't equals");
     }
 
     public function testSlugSetadoCorretamente()
     {
-        $product = (new Product())->setSlug('product-01');
+        $product = ($this->product)->setSlug('product-01');
         
         $this->assertEquals('product-01', $product->getSlug(), "Values aren't equals");
     }
