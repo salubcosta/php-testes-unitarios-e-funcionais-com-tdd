d6f438ec154cab900e10af6af74a5e7ca44a3f8b
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-04-10 23:16:10 -0300

    Criado novos testes...

--- components/querybuilder/tests/QueryBuilder/SelectTest.php
+++ components/querybuilder/tests/QueryBuilder/SelectTest.php
@@ -17,13 +17,19 @@ class SelectTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->select = new Select();
+        $this->select = new Select('products');
     }
 
     public function testIfQueryBaseIsGeneratedWithSuccess()
     {
-        $query = $this->select('products');
+        $query = $this->select->getSql('products');
 
-        $this->assertEquals('SELECT * FROM products', $query->getSql());
+        $this->assertEquals("SELECT * FROM `products`", $query);
+    }
+
+    public function testIfQueryIsGeneratedWithWhereConditions()
+    {
+        $query = $this->select->where('name', '=', 'Product 01', 'concat');
+        $this->assertEquals("SELECT * FROM `products` WHERE name = 'Product 01'", $query->getSql());
     }
 }
