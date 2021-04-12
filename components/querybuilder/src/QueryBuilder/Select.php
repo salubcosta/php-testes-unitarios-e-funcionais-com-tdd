2d3f7f935af1526b573daa64e6aafb7d4f83a1cc
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-04-11 21:07:08 -0300

    Criado metodo orderby e modificado where

--- components/querybuilder/src/QueryBuilder/Select.php
+++ components/querybuilder/src/QueryBuilder/Select.php
@@ -5,21 +5,35 @@ namespace Code\QueryBuilder;
 class Select 
 {
     private $query;
+    private $where;
+    private $orderBy;
 
     public function __construct($table)
     {
         $this->query = "SELECT * FROM `{$table}`";
     }
 
-    public function where($field, $operator, $value)
+    public function where($field, $operator, $value = null, $concat = 'AND')
     {
-        $this->query .= " WHERE {$field} {$operator} '{$value}'";
+        $value = is_null($value) ? ':'.$field : $value;
 
+        if(!$this->where) {
+            $this->where .= " WHERE {$field} {$operator} {$value}";
+        } else {
+            $this->where .= " {$concat} {$field} {$operator} {$value}";
+        }
+
+        return $this;
+    }
+
+    public function orderBy($field, $order)
+    {
+        $this->orderBy = " ORDER BY {$field} {$order}";
         return $this;
     }
 
     public function getSql()
     {
-        return $this->query;
+        return $this->query . $this->where . $this->orderBy;
     }
 }
