623c4d089a345144189092b3a04096c0464e2061
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-04-16 23:31:08 -0300

    Classe para gerar queries de update

---
+++ components/querybuilder/src/QueryBuilder/Update.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Code\QueryBuilder;
+
+class Update
+{
+    private $sql;
+
+    public function __construct(string $table, array $fields = array(), array $conditions = array(), string $operator = "AND")
+    {
+        $this->sql = "UPDATE `{$table}` SET ";
+
+        $set = array();
+        foreach($fields as $f) {
+            $set[] = "{$f} = :{$f}";
+        }
+
+        $where = "";
+        foreach($conditions as $key => $c) {
+            $where .= $where ? "{$operator} {$key} = {$c}" : " WHERE {$key} = {$c}";
+        }
+        $this->sql .= implode(', ', $set).$where;
+    }
+
+    public function getSql()
+    {
+        return $this->sql;
+    }
+}
