776ba70422d79ae0671d5b43f92c531b172b736d
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-04-17 22:54:59 -0300

    Criado classe para gerar delete

---
+++ components/querybuilder/src/QueryBuilder/Delete.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Code\QueryBuilder;
+
+use PhpParser\Node\Stmt\Foreach_;
+
+class Delete
+{
+    private $sql;
+
+    public function __construct(string $table, array $condtitions, string $logicOperator = 'AND')
+    {
+        $this->sql = "DELETE FROM `{$table}` WHERE ";
+
+        $where = "";
+        foreach($condtitions as $key => $value) {
+            $where .= $where ? " {$logicOperator} {$key} = {$value}" : "{$key} = {$value}";
+        }
+
+        $this->sql .= $where;
+    }
+
+    public function getSql()
+    {
+        return $this->sql;
+    }
+}
