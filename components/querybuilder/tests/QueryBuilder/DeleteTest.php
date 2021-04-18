94ee52269f92d850c18c28fb87bc57f39369333a
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-04-17 22:55:26 -0300

    Classe de teste de Delete

---
+++ components/querybuilder/tests/QueryBuilder/DeleteTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PhpParser\Node\Name;
+
+namespace CodeTests\QueryBuilder;
+
+use PHPUnit\Framework\TestCase;
+use Code\QueryBuilder\Delete;
+
+class DeleteTest extends TestCase
+{
+    private $delete;
+
+    protected function assertPreConditions(): void
+    {
+        $this->assertTrue(class_exists(Delete::class));
+    }
+
+    protected function setUp(): void
+    {
+        $this->delete = new Delete('products', ['id' => 10, 'qtd' => -1]);
+    }
+
+    public function testIfDeleteQueryHasGeneratedWithSuccess()
+    {
+        $sql = "DELETE FROM `products` WHERE id = 10 AND qtd = -1";
+        
+        $this->assertEquals($sql, $this->delete->getSql());
+    }
+}
