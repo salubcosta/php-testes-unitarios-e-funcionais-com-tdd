eb48468e5741649bd8ddf0ef7c4ef25e83b91d1e
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-04-19 23:41:29 -0300

    criado primeiro teste para inserir dados dentro do banco de dados sqlite

--- components/querybuilder/tests/QueryBuilder/ExecutorTest.php
+++ components/querybuilder/tests/QueryBuilder/ExecutorTest.php
@@ -3,10 +3,13 @@
 namespace CodeTests\QueryBuilder;
 
 use PHPUnit\Framework\TestCase;
+use Code\QueryBuilder\Executor;
+use Code\QueryBuilder\Query\Insert;
 
 class ExecutorTest extends TestCase
 {
     private static $conn;
+    private static $executor;
 
     public static function setUpBeforeClass(): void 
     {
@@ -20,10 +23,25 @@ class ExecutorTest extends TestCase
                 'updated_at' TIMESTAMP
             );
         ");
+
+        self::$executor = new Executor(self::$conn);
     }
 
     public static function tearDownAfterClass(): void
     {
         self::$conn->exec("DROP TABLE IF EXISTS 'products'");
+    }
+
+    public function testInsertANewProductInADatabase()
+    {
+        $query = new Insert('products', ['name', 'price', 'created_at', 'updated_at']);
+
+        self::$executor->setQuery($query->getSql());
+        self::$executor->setParam(':name', 'Product 1')
+                       ->setParam(':price', 19.99)
+                       ->setParam(':created_at', date('Y-m-d H:i:s'))
+                       ->setParam(':updated_at', date('Y-m-d H:i:s'))
+        ;
+        self::$executor->execute();
     }
 }
